42992831067a65a817f8ea957feb510573edff78
Benjamin Domingo <benjamin.domingo@learnet.se>
2025-01-29 13:50:23 +0100

    Comments, Transition works in correct time

--- Crumbs of the Cookie/Assets/Scripts/SceneTransition.cs
+++ Crumbs of the Cookie/Assets/Scripts/SceneTransition.cs
@@ -14,9 +14,11 @@ public class SceneTransition : MonoBehaviour
 
     IEnumerator FadeInCor()
     {
-        for (float i = 0; i < transitionDuration; i += Time.deltaTime)
+        for (float i = 0; i < transitionDuration; i += Time.deltaTime/transitionDuration)
         {
             Debug.Log(i);
+            //slowly lowers the opacity (last number of the color vector) from 1 (Fully black) to 0 (invisible)
+            //WaitForEndOfFrame stops it from triggering multiple times in a frame
             spriteRenderer.color = new Color(0f, 0f, 0f, 1-i);
             yield return new WaitForEndOfFrame();
         }
@@ -28,7 +30,7 @@ public class SceneTransition : MonoBehaviour
 
     IEnumerator FadeOutCor()
     {
-        for (float i = 0; i < transitionDuration; i += Time.deltaTime)
+        for (float i = 0; i < transitionDuration; i += Time.deltaTime/transitionDuration)
         {
             Debug.Log(i);
             spriteRenderer.color = new Color(0f, 0f, 0f, i);
