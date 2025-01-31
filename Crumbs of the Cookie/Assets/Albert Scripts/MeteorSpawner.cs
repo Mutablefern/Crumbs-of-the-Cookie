dadf8e1811a61f2f3ccba59c2c330153d572590b
Benjamin Domingo <benjamin.domingo@learnet.se>
2025-01-31 11:00:27 +0100

    Added option for Meteors to randomly target the ground

--- Crumbs of the Cookie/Assets/Albert Scripts/MeteorSpawner.cs
+++ Crumbs of the Cookie/Assets/Albert Scripts/MeteorSpawner.cs
@@ -1,8 +1,17 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MeteorSpawner : MonoBehaviour
 {
-    [SerializeField] GameObject strikeTarget; //the target for the meteor
+    public  enum MeteorType
+    {
+        Ground,
+        House
+    }
+
+    public MeteorType MeteorTarget;
+
+    [SerializeField] List <GameObject> strikeTarget; //the target for the meteor
     [SerializeField] int meteorOrigin; //where the meteor is spawned (X)
     [SerializeField] int spawnHeight; //where the meteor is spawned (Y)
     [SerializeField] bool containsEnemy; //if the meteor has enemies inside
@@ -11,19 +20,34 @@ public class MeteorSpawner : MonoBehaviour
     Vector3 spawnLocation; //where the meteor is spawned, calculated using meteorOrigin
     Quaternion spawnRotation; //the rotation of the meteor when spawned
 
-    void Start()
-    {
-        spawnLocation = new Vector3(meteorOrigin, 10, 0); //calculates spawnLocation
-        spawnRotation = new Quaternion(0, 0, 0, 0);
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player" && active) //Spawns meteor when a player reaches the spawner
         {
             active = false; //Deactivates spawner
-            GameObject spawnedMeteor = Instantiate(meteor, spawnLocation, spawnRotation);
-            spawnedMeteor.GetComponent<MeteorController>().changeTarget(strikeTarget.transform);
+            switchMeteor();
         }
     }
+
+    public void switchMeteor()
+    {
+        switch (MeteorTarget)
+        {
+            case MeteorType.Ground:
+                spawnLocation = new Vector3(Random.Range(-10, 10), spawnHeight, 0); //calculates spawnLocation
+                GameObject spawnedMeteorGround = Instantiate(meteor, spawnLocation, Quaternion.identity);
+                MeteorController MeteorControllerScriptGround = spawnedMeteorGround.GetComponent<MeteorController>();
+                MeteorControllerScriptGround.changeTarget(strikeTarget[Random.Range(2, strikeTarget.Count)].transform);
+                MeteorControllerScriptGround.MeteorSpawner = gameObject;
+                break;
+
+            case MeteorType.House:
+                spawnLocation = new Vector3(meteorOrigin, spawnHeight, 0);
+                GameObject spawnedMeteorHouse = Instantiate(meteor, spawnLocation, Quaternion.identity);
+                MeteorController MeteorControllerScriptHouse = spawnedMeteorHouse.GetComponent<MeteorController>();
+                MeteorControllerScriptHouse.changeTarget(strikeTarget[1].transform);
+                break;
+        }
+    }
+
 }
