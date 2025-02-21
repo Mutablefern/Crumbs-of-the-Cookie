d2e19bec8902423411646e9d633d7e21610ab063
Benjamin Domingo <benjamin.domingo@learnet.se>
2025-02-21 08:46:36 +0100

    Reapply "Merge branch 'Viggo-Branch'"
    This reverts commit 68f4aa928227d689db05f4fa545915e8fb33079c.

---
+++ Crumbs of the Cookie/Assets/Viggo Folder/Scripts/EnemyPointMovement.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class EnemyPointMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+
+    public GameObject PointA;
+    public GameObject PointB;
+
+    Transform currentPoint;
+    Rigidbody2D rb_Enemy;
+
+    void Awake()
+    {
+        rb_Enemy = GetComponent<Rigidbody2D>();
+        currentPoint = PointB.transform;
+    }
+
+    void Update()
+    {
+        //Calculate direction and move
+        Vector2 direction = (currentPoint.position - transform.position).normalized;
+        rb_Enemy.linearVelocity = direction * moveSpeed;
+
+        //Check proximity to switch points
+        if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f)
+        {
+            flip();
+            currentPoint = currentPoint == PointB.transform ? PointA.transform : PointB.transform;
+        }
+    }
+
+    void flip()
+    {
+        //Flips the game object
+        Vector3 localscale = transform.localScale;
+        localscale.x *= -1;
+        transform.localScale = localscale;
+    }
+
+    void OnDrawGizmos()
+    {
+        if (PointA != null && PointB != null)
+        {
+            Gizmos.DrawWireSphere(PointA.transform.position, 0.5f);
+            Gizmos.DrawWireSphere(PointB.transform.position, 0.5f);
+            Gizmos.DrawLine(PointA.transform.position, PointB.transform.position);
+        }
+    }
+}
