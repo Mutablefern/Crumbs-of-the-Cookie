2580bc4278333ec9ef56eeca344fb7147a95dfc0
Snabbsly <herberthaegg@outlook.com>
2025-03-05 11:26:49 +0100

    Started making knockback

--- Crumbs of the Cookie/Assets/Benjamin Folder/Scripts/EnemyHealth.cs
+++ Crumbs of the Cookie/Assets/Benjamin Folder/Scripts/EnemyHealth.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyHealth : MonoBehaviour
@@ -13,10 +14,18 @@ public class EnemyHealth : MonoBehaviour
     [Tooltip("3 or less for rat, more for bear")]
     [SerializeField] int health;
     ParticlesManager particlesManager;
+    EnemyPointMovement enemyPointMovement;
+
+
+
+
 
     private void Start()
     {
+
         particlesManager = GameObject.Find("Particle Manager").GetComponent<ParticlesManager>();
+        enemyPointMovement= GetComponent<EnemyPointMovement>();
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -38,10 +47,22 @@ public class EnemyHealth : MonoBehaviour
     }
     void DamageEffect()
     {
-        if (thisEnemyType==enemyType.GummyBear || thisEnemyType == enemyType.GummyRat)
+        if (thisEnemyType==enemyType.GummyBear)
+        {
+            particlesManager.Particels(1, transform.position);
+            StartCoroutine(KnockBack());
+
+        }
+        if (thisEnemyType == enemyType.GummyRat)
         {
             particlesManager.Particels(1, transform.position);
         }
     }
+    IEnumerator KnockBack()
+    {
+        enemyPointMovement.enemyState = 2;
+        yield return new WaitForSeconds(0.5f);
+        enemyPointMovement.enemyState = 1;
 
+    }
 }
--- Crumbs of the Cookie/Assets/Viggo Folder/Scripts/EnemyPointMovement.cs
+++ Crumbs of the Cookie/Assets/Viggo Folder/Scripts/EnemyPointMovement.cs
@@ -1,27 +1,31 @@
+using System.Collections;
 using UnityEngine;
 
 public class EnemyPointMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 1f;
+    [SerializeField] int knockback;
+    [SerializeField] int knockbackY;
+    public int enemyState;
 
     public GameObject PointA;
     public GameObject PointB;
 
     Transform currentPoint;
     Rigidbody2D rb_Enemy;
+    
+    Vector2 direction;
+   
 
     void Awake()
     {
+        enemyState = 1;
         rb_Enemy = GetComponent<Rigidbody2D>();
         currentPoint = PointB.transform;
     }
 
     void Update()
     {
-        //Calculate direction and move
-        Vector2 direction = (currentPoint.position - transform.position).normalized;
-        rb_Enemy.linearVelocity = direction * moveSpeed;
-
         //Check proximity to switch points
         if (Vector2.Distance(transform.position, currentPoint.position) < 0.5f)
         {
@@ -29,7 +33,47 @@ public class EnemyPointMovement : MonoBehaviour
             currentPoint = currentPoint == PointB.transform ? PointA.transform : PointB.transform;
         }
     }
+    private void FixedUpdate()
+    {
+       switch(enemyState)
+        {
+            case 1:
 
+                //Calculate direction and move
+                direction = (currentPoint.position - transform.position).normalized;
+                rb_Enemy.linearVelocity = direction * moveSpeed;
+
+                break;
+
+            case 2:
+
+                //Enemy knockback
+
+                Knockback();
+
+                break;
+
+
+        }
+
+     
+        
+        
+
+
+
+    }
+    void Knockback()
+    {
+        Debug.Log("knockback");
+            StartCoroutine(KnockbackCor());
+    }
+    IEnumerator KnockbackCor()
+    {
+        yield return new WaitForSeconds(0.5f);
+        rb_Enemy.AddForce(direction * knockback, ForceMode2D.Impulse);
+        rb_Enemy.AddForce(transform.up * knockbackY, ForceMode2D.Impulse);
+    }
     void flip()
     {
         //Flips the game object
