5744efaf767243e7f8018bcd0d654958b983ddbc
Snabbsly <herberthaegg@outlook.com>
2025-03-14 10:54:43 +0100

    fixed a jump for the ledgecheck enemy

--- Crumbs of the Cookie/Assets/Viggo Folder/Scripts/EnemyLedgeChecking.cs
+++ Crumbs of the Cookie/Assets/Viggo Folder/Scripts/EnemyLedgeChecking.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -10,15 +12,17 @@ public class EnemyLedgeChecking : MonoBehaviour
     [SerializeField] int knockback;
     [SerializeField] int knockbackY;
     [SerializeField] LayerMask groundLayer;
-    public int enemyState;
     [SerializeField] Animator enemyAnim;
-
+    [SerializeField] float sightRange;
+    [SerializeField] int jumpForce;
     [SerializeField] bool isFacingRight;
 
+    public int enemyState;
     Rigidbody2D rb_Enemy;
+    public Transform playerTransform;
     EnemyHealth enemyHealth;
     Vector3 direction;
-   
+    bool shouldJump;
 
     void Awake()
     {
@@ -42,48 +46,75 @@ public class EnemyLedgeChecking : MonoBehaviour
                 break;
 
             case 2:
+                // used for knockback whenever the enemy gets hit
 
-                
 
                 break;
 
+            case 3:
+
+
+
+                break;
         }
-    } 
+    }
 
     public void Knockback()
     {
+        rb_Enemy.linearVelocityY = 0;
         direction = transform.position - enemyHealth.sourceOfKnockback;
         rb_Enemy.AddForce(direction.normalized * knockback, ForceMode2D.Impulse);
         rb_Enemy.AddForce(transform.up * knockbackY, ForceMode2D.Impulse);
+
     }
-        void Move()
+    void Move()
+    {
+        DetectPlayer();
+        JumpAttack();
+
+       
+
+        if (CheckGrounded())
         {
-            if (CheckGrounded())
-            {
-                rb_Enemy.linearVelocityX = transform.right.x * moveSpeed;
-            }
-            else
-            {
-                rb_Enemy.linearVelocityX = 0f;
-            }
+            rb_Enemy.linearVelocityX = transform.right.x * moveSpeed;
         }
-
-        void LedgeCheck()
+        else
         {
-            //Checking if ledge infront
-            RaycastHit2D hit = Physics2D.Raycast(
-                ledgeCheckPosition.position,
-                Vector2.down,
-                ledgeCheckLength,
-                groundLayer);
-
-            if (hit.collider == null && CheckGrounded())
-            {
-                Flip();
-            }
+            rb_Enemy.linearVelocityX = 0f;
         }
-    
+    }
 
+    void LedgeCheck()
+    {
+        //Checking if there is a ledge infrot of the enemy
+        RaycastHit2D hit = Physics2D.Raycast(
+            ledgeCheckPosition.position,
+            Vector2.down,
+            ledgeCheckLength,
+            groundLayer);
+
+        if (hit.collider == null && CheckGrounded())
+        {
+            Flip();
+        }
+    }
+    void DetectPlayer()
+    {
+        //is player within sight range?
+        if (Vector2.Distance(transform.position, playerTransform.position) <= sightRange)
+        {
+           
+            StartCoroutine(AttackWindow());
+        }
+        
+    }
+
+    IEnumerator AttackWindow()
+    {
+        shouldJump = true;
+        yield return new WaitForSeconds(0.5f);
+        shouldJump = false;
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -124,4 +155,26 @@ public class EnemyLedgeChecking : MonoBehaviour
             transform.eulerAngles = new Vector3(0f, -180f, 0f);
         }
     }
+
+    void JumpAttack()
+    {
+        if (CheckGrounded() && shouldJump)
+        {
+            StartCoroutine(AttackCor());
+           
+        }
+    }
+    
+    IEnumerator AttackCor()
+    {
+        
+        enemyState = 3;
+        enemyAnim.SetTrigger("RatAttack");
+        Vector2 direction = (playerTransform.position - transform.position).normalized;
+        Vector2 jumpDirection = direction * jumpForce;
+        rb_Enemy.AddForce(new Vector2(jumpDirection.x, jumpForce), ForceMode2D.Impulse);
+        yield return new WaitForSeconds(0.5f);
+        enemyState = 1; 
+    }
+    
 }
