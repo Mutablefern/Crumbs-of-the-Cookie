0906a93f53ec1ee052ddd813f3d6d563f866f7da
MatiasMC1 <matis123457@hotmail.com>
2025-03-21 10:56:56 +0100

    Kopplar animation just nu

---
+++ Crumbs of the Cookie/Assets/Viggo Folder/Scripts/EnemyLedgeCheckingRollyPolly.cs
@@ -0,0 +1,107 @@
+using UnityEngine;
+
+public class EnemyLedgeCheckingRollyPolly : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    [SerializeField] Transform ledgeCheckPosition;
+    [SerializeField] float ledgeCheckLength;
+    [SerializeField] float groundCheck = 1f;
+    [SerializeField] LayerMask groundLayer;
+    [SerializeField] Transform playerTransform;
+    public float sightRange;
+
+
+    public bool playerDetected;
+    bool isFacingRight;
+    
+
+    Rigidbody2D rb_Enemy;
+
+    void Awake()
+    {
+        rb_Enemy = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        LedgeCheck();
+    }
+
+    private void Update()
+    {
+        DetectPlayer();
+    }
+
+    void Move()
+    {
+        if (CheckGrounded())
+        {
+            rb_Enemy.linearVelocityX = transform.right.x * moveSpeed;
+        }
+        else
+        {
+            rb_Enemy.linearVelocityX = 0f;
+        }
+    }
+
+    void LedgeCheck()
+    {
+        //Checking if ledge infront
+        RaycastHit2D hit = Physics2D.Raycast(
+            ledgeCheckPosition.position,
+            Vector2.down,
+            ledgeCheckLength,
+            groundLayer);
+
+        if (hit.collider == null && CheckGrounded())
+        {
+            isFacingRight = !isFacingRight;
+
+            if (isFacingRight)
+            {
+                transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0f, -180f, 0f);
+            }
+        }
+    }
+
+    bool CheckGrounded()
+    {
+        Collider2D isGrounded = Physics2D.OverlapCircle(transform.position, groundCheck, groundLayer);
+
+        return isGrounded;
+
+    }
+
+
+    void DetectPlayer()
+    {
+        //is player within sight range?
+        if (Vector2.Distance(transform.position, playerTransform.position) <= sightRange)
+        {
+            playerDetected = true;
+        }
+        else
+        {
+            playerDetected = false;
+        }
+    }
+
+    void RollMode()
+    {
+        if (playerDetected == true)
+        {
+            
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, groundCheck);
+    }
+}
