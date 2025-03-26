07e957b4a21d19747172a6470bac0276b2f20689
MatiasMC1 <matis123457@hotmail.com>
2025-03-26 11:25:40 +0100

    Rolly Polly
    ADEED SHIIIITT TWINNNN GRAAAA

--- Crumbs of the Cookie/Assets/Viggo Folder/Scripts/EnemyLedgeCheckingRollyPolly.cs
+++ Crumbs of the Cookie/Assets/Viggo Folder/Scripts/EnemyLedgeCheckingRollyPolly.cs
@@ -8,64 +8,96 @@ public class EnemyLedgeCheckingRollyPolly : MonoBehaviour
     [SerializeField] float groundCheck = 1f;
     [SerializeField] LayerMask groundLayer;
     [SerializeField] Transform playerTransform;
+    [SerializeField] float minMoveSpeed;
+    [SerializeField] float maxMoveSpeed;
+    [SerializeField] int knockback;
+    [SerializeField] int knockbackY;
+    public int enemyState;
     public float sightRange;
-
-
+    Animator animator;
+    EnemyHealth enemyHealth;
+    Vector3 direction;
     public bool playerDetected;
     bool isFacingRight;
-    
+
+    public GameObject[] wayPoints;
+    public int nextWayPoint = 1;
+    float distToPoint;
 
     Rigidbody2D rb_Enemy;
 
     void Awake()
     {
+        enemyState = 1;
         rb_Enemy = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
+        enemyHealth = GetComponent<EnemyHealth>();
     }
 
     void FixedUpdate()
     {
-        Move();
-        LedgeCheck();
+
+        switch (enemyState)
+        {
+            case 1:
+
+                Move();
+                break;
+
+            case 2:
+                // used for knockback whenever the enemy gets hit
+
+
+                break;
+
+            case 3:
+
+
+
+                break;
+        }
     }
 
     private void Update()
     {
         DetectPlayer();
+        RollMode();
+    }
+    public void Knockback()
+    {
+        rb_Enemy.linearVelocityY = 0;
+        direction = transform.position - enemyHealth.sourceOfKnockback;
+        rb_Enemy.AddForce(direction.normalized * knockback, ForceMode2D.Impulse);
+        rb_Enemy.AddForce(transform.up * knockbackY, ForceMode2D.Impulse);
     }
 
     void Move()
     {
-        if (CheckGrounded())
+        distToPoint = Vector2.Distance(transform.position, wayPoints[nextWayPoint].transform.position);
+
+        transform.position = Vector2.MoveTowards(transform.position, wayPoints[nextWayPoint].transform.position, moveSpeed * Time.deltaTime);
+
+        if (distToPoint < 0.2f)
         {
-            rb_Enemy.linearVelocityX = transform.right.x * moveSpeed;
-        }
-        else
-        {
-            rb_Enemy.linearVelocityX = 0f;
+            TakeTurn();
         }
     }
 
-    void LedgeCheck()
+    void TakeTurn()
     {
-        //Checking if ledge infront
-        RaycastHit2D hit = Physics2D.Raycast(
-            ledgeCheckPosition.position,
-            Vector2.down,
-            ledgeCheckLength,
-            groundLayer);
+        Vector3 currRot = transform.eulerAngles;
+        currRot.z += wayPoints[nextWayPoint].transform.eulerAngles.z;
+        transform.eulerAngles = currRot;
+        ChooseNextWayPoint();
+    }
 
-        if (hit.collider == null && CheckGrounded())
+    void ChooseNextWayPoint()
+    {
+        nextWayPoint++;
+
+        if(nextWayPoint == wayPoints.Length)
         {
-            isFacingRight = !isFacingRight;
-
-            if (isFacingRight)
-            {
-                transform.eulerAngles = new Vector3(0f, 0f, 0f);
-            }
-            else
-            {
-                transform.eulerAngles = new Vector3(0f, -180f, 0f);
-            }
+            nextWayPoint = 0;
         }
     }
 
@@ -76,7 +108,6 @@ public class EnemyLedgeCheckingRollyPolly : MonoBehaviour
         return isGrounded;
 
     }
-
 
     void DetectPlayer()
     {
@@ -95,9 +126,39 @@ public class EnemyLedgeCheckingRollyPolly : MonoBehaviour
     {
         if (playerDetected == true)
         {
-            
+            animator.SetBool("isRolling", true);
+            moveSpeed = maxMoveSpeed;
+        }
+        else if (playerDetected == false)
+        {
+            animator.SetBool("isRolling", false);
+            moveSpeed = minMoveSpeed;
         }
     }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        isFacingRight = !isFacingRight;
+
+        if (isFacingRight)
+        {
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        }
+        else
+        {
+            transform.eulerAngles = new Vector3(0f, -180f, 0f);
+        }
+    }
+
 
     void OnDrawGizmos()
     {
