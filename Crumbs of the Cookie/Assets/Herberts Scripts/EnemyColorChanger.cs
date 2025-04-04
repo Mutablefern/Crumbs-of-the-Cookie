f514ccf34f2ef8f6f1ecbf46e0495574cdff82ff
Snabbsly <herberthaegg@outlook.com>
2025-04-04 09:03:44 +0200

    Finished the gummy enemies

--- Crumbs of the Cookie/Assets/Herberts Scripts/EnemyColorChanger.cs
+++ Crumbs of the Cookie/Assets/Herberts Scripts/EnemyColorChanger.cs
@@ -3,11 +3,40 @@ using UnityEngine;
 public class EnemyColorChanger : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-
+    Renderer enemy_Renderer;
 
     void Start()
     {
-        
+        enemy_Renderer = GetComponentInChildren<Renderer>();
+        ColorChanger();
+       
+    }
+
+    void ColorChanger()
+    {
+         
+        int colorValue = Random.Range(0,4);
+        if (colorValue == 0)
+        {
+            enemy_Renderer.material.SetColor("_Color", Color.green);
+        }
+        if (colorValue == 1)
+        {
+            enemy_Renderer.material.SetColor("_Color", Color.red);
+        }
+        if (colorValue == 2)
+        {
+            enemy_Renderer.material.SetColor("_Color", Color.cyan);
+        }
+        if (colorValue == 3)
+        {
+            enemy_Renderer.material.SetColor("_Color", Color.magenta);
+        }
+        if (colorValue == 4)
+        {
+            enemy_Renderer.material.SetColor("_Color", Color.yellow);
+        }
+       
     }
 
     // Update is called once per frame
