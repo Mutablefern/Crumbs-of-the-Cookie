90218b4ac7b33ddd2c6e3ace0fed9c6e62d3e156
MatiasMC1 <matis123457@hotmail.com>
2025-04-23 10:51:54 +0200

    level fixing
    just working on the level

--- Crumbs of the Cookie/Assets/Matias Map/Scripts/BounceMellowBounce.cs
+++ Crumbs of the Cookie/Assets/Matias Map/Scripts/BounceMellowBounce.cs
@@ -4,13 +4,15 @@ public class BounceMellowBounce : MonoBehaviour
 {
     public float maxVelocity = 35f;
     private float bounceVelocity = 1f;
-    private float Bounciness;
+    private float bounceness;
     public Rigidbody2D player_rb;
     public float guaranteedBounce = 20f;
+    public bool touchedBounceMellow;
+    public float waitForTime;
 
     private void FixedUpdate()
     {
-        if (player_rb.linearVelocityY > 45f)
+        if (player_rb.linearVelocityY > maxVelocity)
         {
             player_rb.linearVelocity = Vector2.ClampMagnitude(player_rb.linearVelocity, maxVelocity);
         }
@@ -20,20 +22,26 @@ public class BounceMellowBounce : MonoBehaviour
     {
         bounceVelocity = player_rb.linearVelocityY;
 
-        Bounciness = bounceVelocity;
+        bounceness = bounceVelocity;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * Mathf.Abs(Bounciness), ForceMode2D.Impulse);
-            
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * Mathf.Abs(bounceness), ForceMode2D.Impulse);
+            touchedBounceMellow = true;
         }
+    }
 
+    private void OnCollisionExit2D(Collision2D collision)
+    {
         if (collision.gameObject.CompareTag("Player"))
-        {    
-            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * guaranteedBounce, ForceMode2D.Impulse);
+        {
+            if (touchedBounceMellow == true)
+            {
+                collision.gameObject.GetComponent<Rigidbody2D>().AddForce(Vector2.up * guaranteedBounce, ForceMode2D.Impulse);
+            }
         }
     }
 }
